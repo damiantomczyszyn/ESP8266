6fec9c124d678aa3c532c690543288239fe20afe
Damian Tomczyszyn <damitom@o2.pl>
2024-01-12 15:17:57 +0100

    get Data from local server

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 //#include <Arduino_JSON.h>
+#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "s";     // your network SSID (name of wifi network)
+const char* ssid     = "";     // your network SSID (name of wifi network)
 const char* password = ""; // your network password
 
 const char*  server = "weather.visualcrossing.com";  // Server URL
 WiFiClientSecure client;
-
+String getHtmlContent(const char* url);
+void getDataFromStation();
 //const size_t capacity = JSON_OBJECT_SIZE(1000) ;
 //StaticJsonDocument<capacity> doc;
 const size_t capacity = JSON_OBJECT_SIZE(1000) + 300;
@@ -53,7 +55,7 @@ void setup() {
   else {
     Serial.println("Connected to server!");
     // Make a HTTP request:
-    client.println("GET https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lublin/2024-01-10/2024-01-14?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cpressure%2Ccloudcover%2Cuvindex%2Csunrise%2Csunset&include=fcst%2Cremote%2Cobs%2Cdays&key= HTTP/1.0");
+    client.println("GET https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lublin/2024-01-10/2024-01-14?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cpressure%2Ccloudcover%2Cuvindex%2Csunrise%2Csunset&include=fcst%2Cremote%2Cobs%2Cdays&key=&contentType=json HTTP/1.0");
     client.println("Host: weather.visualcrossing.com");
     client.println("Connection: close");
     client.println();
@@ -122,6 +124,14 @@ for (int x = 0; x < 5; x++) {
     printDayInfo(days, x + 1);
   }
 
+  Serial.println();
+    // Pobranie treści strony HTML
+ // String htmlContent = getHtmlContent("192.168.101.76");
+ // Serial.println("HTML Content:");
+ // Serial.println(htmlContent);
+
+
+  getDataFromStation();
 
 
 }
@@ -173,4 +183,91 @@ void printDayInfo(JsonObject day, int dayNumber) {
   Serial.println("Sunrise: " + String(sunrise));
   Serial.println("Sunset: " + String(sunset));
   Serial.println();
+}
+
+
+String getHtmlContent(const char* url) {
+  String content = "";
+  WiFiClientSecure client;
+  Serial.println("\nStarting connection to server...");
+  
+
+  if (!client.connect(url, 80))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+    // Make a HTTP request:
+    client.println("GET http://192.168.101.76/ HTTP/1.0");
+    client.println("Host:" + String(url));
+    client.println("Connection: close");
+    client.println();
+  // Rozpoczęcie sesji HTTP
+ 
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      
+      Serial.println(line); // HTTP/1.1 200
+    }
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    Serial.println("teraz znaki");
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+      content+=c;
+    }
+    Serial.println();
+    client.stop();
+  }
+  return content;
+  
+}
+
+void getDataFromStation(){
+  //Your Domain name with URL path or IP address with path
+String serverName = "http://192.168.101.76";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+    //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      WiFiClient client; // Utwórz obiekt WiFiClien
+      String serverPath = serverName;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverPath.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      Serial.println("wykonanie get do servera http");
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
